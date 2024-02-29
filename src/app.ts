ce9c9067d8bc8e393cc2aba57c15cfd2c7236a6b
Ashfaq Salehin <ashfaq.aws@gmail.com>
2024-02-29 21:32:24 +0000

    Add routes and server

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+// Controllers (route handlers)
+import * as homeController from "./controllers/home";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController.index);
+
+export default app;
